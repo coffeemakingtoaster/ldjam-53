88952abe3df0417acb931a75d9f58a49443cc7b3
Ben Häußermann <ben.haussermann@gmail.com>
2023-04-29 16:58:56 +0200

    moving cars

---
+++ Assets/CarController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public GameObject target; // The current target GameObject for the car
+    public float speed = 5f; // The speed at which the car moves towards the target
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            Vector3 targetPosition = target.transform.position;
+            
+            
+            // Rotate the car to face the offset position
+            Vector3 direction = targetPosition - transform.position;
+            transform.rotation = Quaternion.LookRotation(direction);
+            
+            // Move the car towards the offset position at the specified speed
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            // Check if the car has reached the target
+            if (Vector3.Distance(transform.position, target.transform.position) < 0.5f)
+            {
+                // Set the target to a random neighboring MapMarker
+                MapMarker mapMarker = target.GetComponent<MapMarker>();
+                if (mapMarker != null)
+                {
+                    MapMarker[] neighbors = mapMarker.neighbors;
+                    MapMarker randomNeighbor = neighbors[Random.Range(0, neighbors.Length)];
+                    SetTarget(randomNeighbor.gameObject);
+                }
+            }
+        }
+    }
+
+    public void SetTarget(GameObject newTarget)
+    {
+        target = newTarget;
+    }
+}
---
+++ Assets/MapMarker.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMarker : MonoBehaviour
+{
+      public MapMarker[] neighbors; // 2D array of neighboring MapMarker GameObjects and empty GameObjects
+      
+}
