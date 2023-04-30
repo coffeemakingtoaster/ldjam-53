3f5621be25d52b4197cb69f9858be1a96655c084
Ben Häußermann <ben.haussermann@gmail.com>
2023-04-30 11:01:49 +0200

    Urp and other changes

--- Assets/DropOffPoint.cs
+++ Assets/DropOffPoint.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class DropOffPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string addressName;
+    
 }
